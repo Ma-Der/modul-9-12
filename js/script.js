7ed58ee13c2b84f36c099fc8505ff868c4102965
Maciej Derewiański <eco_derma@poczta.onet.pl>
2019-02-06 14:37:48 +0100

    Fix bugs, Add modal

--- js/script.js
+++ js/script.js
@@ -69,3 +69,44 @@ const postbackPage = document.querySelectorAll('.postback');
       mainPage.innerHTML = loadPage('./src/postback.html');
     });
   }
+
+// MODALS //
+
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show-modal');
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    closeModal();
+  });
+});
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+  if(e.target === this) {
+    closeModal();
+  }
+});
+
+document.addEventListener('keyup', function(e) {
+  if(e.keyCode === 27) {
+    closeModal();
+  }
+});
+
+
+function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+    modal.classList.remove('show-modal');
+  });
+  document.querySelector('#overlay').classList.add('show-modal');
+  document.querySelector(modal).classList.add('show-modal');
+}
+
+document.querySelectorAll('.chat').forEach(function(cht) {
+  cht.addEventListener('click', function(e) {
+    e.preventDefault();
+    openModal('#myModal');
+  });
+});

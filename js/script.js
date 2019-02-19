e2506e40445fbc27a0559f22b9531101ece9f854
Maciej Derewiański <eco_derma@poczta.onet.pl>
2019-02-19 17:44:17 +0100

    Add promise in loadPage function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-'use-strict';
+'use strict';
 
 function loadPage(href) {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", href, false);
+  return new Promise((resolve, reject) => {
+  const xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.open("GET", href);
+  xmlhttp.addEventListener('load', () => resolve(xmlhttp.responseText));
+  xmlhttp.addEventListener('error', () => reject(new Error(xmlhttp.statusText)));
   xmlhttp.send();
-  return xmlhttp.responseText;
+});
 }
 
 function toggleMenu(visible) {
@@ -17,56 +21,80 @@ document.querySelector('.mobile-hamburger').addEventListener('click', function(e
 });
 
 document.addEventListener('DOMContentLoaded', function(){
-  mainPage.innerHTML = loadPage('./src/general.html');
+  loadPage('./src/general.html')
+    .then(
+      page => mainPage.innerHTML = page
+    );
 });
 const mainPage = document.getElementById('page');
 
 const generalPage = document.querySelectorAll('.general');
   for(let i=0; i < generalPage.length; i++) {
     generalPage[i].addEventListener('click', function(){
-      mainPage.innerHTML = loadPage('./src/general.html');
+      loadPage('./src/general.html')
+        .then(
+          page => mainPage.innerHTML = page
+        );
     });
   }
 
 const detailsPage = document.querySelectorAll('.details');
   for(let i=0; i < detailsPage.length; i++) {
     detailsPage[i].addEventListener('click', function(){
-      mainPage.innerHTML = loadPage('./src/details.html');
+      loadPage('./src/details.html')
+        .then(
+          page => mainPage.innerHTML = page
+        );
     });
   }
 
 const linksPage = document.querySelectorAll('.links');
   for(let i=0; i < linksPage.length; i++) {
     linksPage[i].addEventListener('click', function(){
-      mainPage.innerHTML = loadPage('./src/links.html');
+      loadPage('./src/links.html')
+        .then(
+          page => mainPage.innerHTML = page
+        );
     });
   }
 
 const bannersPage = document.querySelectorAll('.banners');
   for( let i=0; i < bannersPage.length; i++) {
     bannersPage[i].addEventListener('click', function(){
-      mainPage.innerHTML = loadPage('./src/banners.html');
+      loadPage('./src/banners.html')
+        .then(
+          page => mainPage.innerHTML = page
+        );
     });
   }
 
 const personaldataPage = document.querySelectorAll('.personaldata');
   for(let i=0; i < personaldataPage.length; i++) {
     personaldataPage[i].addEventListener('click', function(){
-      mainPage.innerHTML = loadPage('./src/personaldata.html');
+      loadPage('./src/personaldata.html')
+        .then(
+          page => mainPage.innerHTML = page
+        );
     });
   }
 
 const payoutPage = document.querySelectorAll('.payout');
   for(let i=0; i < payoutPage.length; i++) {
     payoutPage[i].addEventListener('click', function(){
-      mainPage.innerHTML = loadPage('./src/payout.html');
+      loadPage('./src/payout.html')
+        .then(
+          page => mainPage.innerHTML = page
+        );
     });
   }
 
 const postbackPage = document.querySelectorAll('.postback');
   for(let i=0; i < postbackPage.length; i++) {
     postbackPage[i].addEventListener('click', function(){
-      mainPage.innerHTML = loadPage('./src/postback.html');
+      loadPage('./src/postback.html')
+        .then(
+          page => mainPage.innerHTML = page
+        );
     });
   }
 
@@ -110,35 +138,3 @@ document.querySelectorAll('.chat').forEach(function(cht) {
     openModal('#myModal');
   });
 });
-
-// CHART //
-if( DOMContentLoaded) {
-const canvas = document.getElementById('myChart');
-const ctx = canvas.getContext('2d');
-
-const chart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
-    datasets: [{
-      label: "Signups",
-      backgroundColor: '#8DBEC8',
-      borderColor: '#8DBEC8',
-      data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
-    },
-    {
-      label: "FTD",
-      backgroundColor: '#F29E4E',
-      borderColor: '#F29E4E',
-      data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
-    },
-    {
-      label: "Earned",
-      backgroundColor: '#71B374',
-      borderColor: '#71B374',
-      data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
-      hidden: true,
-    }]
-  },
-});
-}
